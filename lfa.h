1e2de9e5033403a1a02b204c3ac25589a9dbc2b9
Jonathan Irwin <jmi@ast.cam.ac.uk>
2022-04-07 17:03:27 +0100

    Removed the old fsincos trick because it's now slower on modern systems. I left in the sincos() on glibc because it didn't seem to be harming anything in my tests, but the compiler is now smart enough to do this itself, and calling explicitly might defeat vectorization so this may need to be reconsidered in the future.

--- lfa.h
+++ lfa.h
@@ -412,32 +412,7 @@ struct wcs_info {
    is called with arguments in (-TWOPI,TWOPI), but on x87 the allowed
    range for the fsincos instruction is much larger than this. */
    
-#if defined(__GNUC__) && (defined(__i386) || defined(__amd64))
-
-/* Inline x86 assembler routines, GNU C syntax */
-#define inline_sincos(a, s, c)   \
-  asm("fsincos"           "\n\t" \
-      "fnstsw  %%ax"      "\n\t" \
-      "btw     $10, %%ax" "\n\t" \
-      "jnc     1f"        "\n\t" \
-      "fldpi"             "\n\t" \
-      "fadd    %%st(0)"   "\n\t" \
-      "fxch    %%st(1)"   "\n\t" \
-"2: " "fprem1"            "\n\t" \
-      "fnstsw  %%ax"      "\n\t" \
-      "btw     $10, %%ax" "\n\t" \
-      "jc      2b"        "\n\t" \
-      "fstp    %%st(1)"   "\n\t" \
-      "fsincos"           "\n\t" \
-"1: "                            \
-      : "=t" (c), "=u" (s)       \
-      : "0" (a)                  \
-      : "ax", "cc")
-
-#define inline_bare_sincos(a, s, c) \
-  asm("fsincos" : "=t" (c), "=u" (s) : "0" (a))
-
-#elif defined(__GLIBC__) && defined(_GNU_SOURCE)
+#if defined(__GLIBC__) && defined(_GNU_SOURCE)
 
 /* Try to use glibc provided routines in math.h */
 #define inline_sincos(a, s, c) sincos((a), &(s), &(c))
